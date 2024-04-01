98082d4f83e5f9345a98f0d495dcdc212fe07d3f
hooni <jhlee909090@gmail.com>
2024-04-01 19:18:11 +0900

    feat: file init, 폰트추가

---
+++ Semina/Semina/WelcomeViewController.swift
@@ -0,0 +1,87 @@
+//
+//  WelcomeViewController.swift
+//  seminar
+//
+//  Created by 이지훈 on 3/30/24.
+//
+
+import UIKit
+import Foundation
+
+final class WelcomeViewController: UIViewController {
+    
+    //MARK: - variable
+    var id : String? = ""
+    
+    //MARK: - 기본 요소들
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 112, y: 87, width: 150, height: 150))
+        imageView.image = UIImage(named: "logo2")
+        return imageView
+    }()
+    
+    private let welcomeLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 140, y: 295, width: 95, height: 60))
+        label.text = "???님 \n반가워요!"
+        label.textAlignment = .center
+        label.numberOfLines = 2
+          label.font = UIFont(name: "Pretendard-ExtraBold", size: 25)
+
+        return label
+    }()
+    
+    private var goHomeButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 426, width: 335, height: 58))
+        button.backgroundColor = UIColor(red: 255/255, green: 111/255, blue: 15/255, alpha: 1)
+        button.setTitle("메인으로", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(backBtnTapped), for: .touchUpInside)
+           button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+
+        return button
+    }()
+    
+    private var backToLoginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 498, width: 335, height: 58))
+        button.backgroundColor = UIColor(red: 221/255, green: 222/255, blue: 227/255, alpha: 1)
+        button.setTitle("로그인하기", for: .normal)
+        button.setTitleColor(UIColor(red: 172/255, green: 176/255, blue: 185/255, alpha: 1), for: .normal)
+        button.addTarget(self, action: #selector(backBtnTapped), for: .touchUpInside)
+             button.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+
+        return button
+    }()
+    
+    
+    //MARK: - ViewDidload
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        setLayout()
+        bindID()
+
+    }
+    
+    private func setLayout() {
+        [logoImageView, welcomeLabel, goHomeButton, backToLoginButton].forEach {
+            //      $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        }
+    }
+    
+    
+    @objc func backBtnTapped() {
+        if self.navigationController == nil {
+            self.dismiss(animated: true)
+        } else {
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func bindID() {
+        guard let idtext = id else { return }
+        self.welcomeLabel.text = "\(idtext)님 \n반가워요!"
+    }
+    
+}
