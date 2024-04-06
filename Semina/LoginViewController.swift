c2bd0497dc4be7d7eea4ea91927d24365820847f
hooni <jhlee909090@gmail.com>
2024-04-06 16:16:08 +0900

    feat: import snapkit, 오토레이아웃 설정

--- Semina/LoginViewController.swift
+++ Semina/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 final class LoginViewController: UIViewController {
     
@@ -21,6 +22,7 @@ final class LoginViewController: UIViewController {
         
         label.font = UIFont(name: "Pretendard-Bold", size: 18)
         return label
+        
     }()
     
     private let idTextField: UITextField = {
@@ -73,6 +75,56 @@ final class LoginViewController: UIViewController {
         [titleLabel, idTextField, passwordTextField, loginButton].forEach {
             self.view.addSubview($0)
         }
+        //
+        //        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        //        idTextField.translatesAutoresizingMaskIntoConstraints = false
+        //        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        //        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        //
+        //        NSLayoutConstraint.activate([titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        //                                     titleLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 161)])
+        //
+        //        NSLayoutConstraint.activate([loginButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 217),
+        //                                     loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        //                                     loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        //                                     loginButton.heightAnchor.constraint(equalToConstant: 58)])
+        //
+        //        NSLayoutConstraint.activate([idTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+        //                                     idTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        //                                     idTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        //                                     idTextField.heightAnchor.constraint(equalToConstant: 52)])
+        //
+        //        NSLayoutConstraint.activate([passwordTextField.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 13),
+        //                                     passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+        //                                     passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        //                                     passwordTextField.heightAnchor.constraint(equalToConstant: 52)])
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(161)
+            make.width.equalTo(236)
+            make.height.equalTo(44)
+        }
+        
+        idTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(71)
+            make.left.right.equalToSuperview().inset(20)
+            make.height.equalTo(52)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(idTextField.snp.bottom).offset(7)
+            make.left.right.equalTo(idTextField)
+            make.height.equalTo(52)
+        }
+        
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(35)
+            make.left.right.equalTo(idTextField)
+            make.height.equalTo(58)
+        }
+        
+        
     }
     
     
--- Semina/WelcomeViewController.swift
+++ Semina/WelcomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Foundation
 
+
 final class WelcomeViewController: UIViewController {
     
     //MARK: - variable
